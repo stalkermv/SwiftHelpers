26618484cd07019d4deccdac35aec4b44e8be582
Valeriy Malishevskyi <stalkermv@ukr.net>
2023-03-23 19:30:06 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftHelpers
+
+A description of this package.
---
+++ Tests/SwiftHelpersTests/SwiftHelpersTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftHelpers
+
+final class SwiftHelpersTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftHelpers().text, "Hello, World!")
+    }
+}
